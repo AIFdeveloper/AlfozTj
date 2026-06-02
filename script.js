a12ba9d4fb3f3c8c3554d8509e6b4f6ad5ce055b
AIFdeveloper <akramovfarhoddeveloper@gmail.com>
2026-06-03 02:04:53 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -456,3 +456,9 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
